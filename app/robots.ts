eb3f640b552f07dafe0759881ecfb0fbe568710c
DabAZ <dabaz@dabaz.me>
2024-07-14 05:38:51 +0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://music.dabaz.me/sitemap.xml',
+    host: 'https://music.dabaz.me',
+  };
+}
