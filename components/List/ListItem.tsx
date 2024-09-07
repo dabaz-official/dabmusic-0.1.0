551c509b12a6a776fb9c9c669f7a523f102cc24a
DabAZ <dabaz@dabaz.me>
2024-09-08 02:23:24 +0800

    Create ListItem.tsx

---
+++ components/List/ListItem.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(href);
+  }
+
+  return (
+    <div>
+      ListItem
+    </div>
+  );
+}
+
+export default ListItem;
