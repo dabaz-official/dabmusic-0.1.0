76224d7adcce2e5f6c7612ca8e90a0a68f4621c4
DabAZ <dabaz@dabaz.me>
2024-09-08 01:04:09 +0800

    add songs of SASIOVERLXRD

---
+++ lib/songs.ts
@@ -0,0 +1,22 @@
+export const songs = [
+  {
+    title: '可惜你不在',
+    artist: 'SASIOVERLXRD',
+    album: '那小子真帅',
+    explicit: true,
+    cover: '/images/songs/那小子真帅.webp',
+    url: '/music/可惜你不在.mp3',
+    lyrics: '/lyrics/可惜你不在.lrc',
+    href: '/songs/kexinibuzai',
+  },
+  {
+    title: '薄荷水',
+    artist: 'SASIOVERLXRD',
+    album: '薄荷水',
+    explicit: false,
+    cover: '/images/songs/薄荷水.jpg',
+    url: '/music/薄荷水.mp3',
+    lyrics: '/lyrics/薄荷水.lrc',
+    href: '/songs/boheshui',
+  },
+]
