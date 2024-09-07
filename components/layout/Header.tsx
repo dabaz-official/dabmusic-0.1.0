604671571a4d7327092c47db295d5456b67a297c
DabAZ <dabaz@dabaz.me>
2024-09-08 01:04:55 +0800

    add header

---
+++ components/layout/Header.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+import { twMerge } from 'tailwind-merge';
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  return (
+    <div 
+      className={twMerge(`
+        h-fit
+      `)}
+    >
+      <div className='w-full mb-4 flex items-center justify-between'>
+        <div className='hidden md:flex gap-x-1 items-center'>
+          <button
+            onClick={() => router.back()}
+            className='rounded-xl hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 transition-colors'
+          >
+            <HiChevronLeft size={20} className='text-neutral-400' />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className='rounded-xl hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 transition-colors'
+          >
+            <HiChevronRight size={20} className='text-neutral-400' />
+          </button>
+        </div>
+        <div className='flex md:hidden gap-x-2 items-center justify-center'>
+          
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+}
+
+export default Header;
